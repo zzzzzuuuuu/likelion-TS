7cb8fa964121d542c00b6994145a0724ebecc822
zzzzzuuuuu <agu0830@naver.com>
2023-11-01 13:47:49 +0900

    feat: interface 사용

--- src/test/TodoList.tsx
+++ src/test/TodoList.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+interface ListItem {
+  id: number;
+  text: string;
+}
+
 const TodoList = () => {
   const [text, setText] = useState<string>('');
-  const [list, setList] = useState<string[]>([]);
+  const [list, setList] = useState<ListItem[]>([]);
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
-  const handleClick = (value: string) => {
-    setList([...list, value]); // list에 value 추가
+  const handleClick = (text: string) => {
     if (text !== '') {
-      list.push(text);
-      setList(list);
+      setList((currentList) => [...currentList, { id: Date.now(), text }]);
       setText('');
     }
     console.log(list);
@@ -26,45 +29,53 @@ const TodoList = () => {
 
   return (
     <>
-      <Title>ToDoList</Title>
-      {/*map 함수는 소괄호..*/}
-      {list.map((list, index) => (
-        <Lists key={index}>{list}</Lists>
-      ))}
-      <input
-        type="text"
-        value={text}
-        placeholder="할 일을 입력해주세요."
-        onChange={handleInput}
-        onKeyDown={handleKeyPress}
-      />
-      <button
-        type="submit"
-        value={text}
-        onClick={() => {
-          handleClick(text);
-        }}
-      >
-        등록하기
-      </button>
+      <TodoContainer>
+        <Title>ToDoList</Title>
+        {list.map((item) => (
+          <Lists key={item.id}>{item.text}</Lists>
+        ))}
+        <SubmitContainer>
+          <Input
+            type="text"
+            value={text}
+            placeholder="할 일을 입력해주세요."
+            onChange={handleInput}
+            onKeyDown={handleKeyPress}
+          />
+          <button
+            type="submit"
+            value={text}
+            onClick={() => {
+              handleClick(text);
+            }}
+          >
+            +
+          </button>
+        </SubmitContainer>
+      </TodoContainer>
     </>
   );
 };
 
 export default TodoList;
 
+const TodoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const Title = styled.h1`
-  color: black;
+  margin-bottom: 20px;
 `;
 
 const Lists = styled.p`
-  color: black;
+  margin: 0;
+`;
+
+const SubmitContainer = styled.div`
+  display: flex;
 `;
 
 const Input = styled.input`
   width: 150px;
-`;
-
-const TextButton = styled.button`
-  width: 70px;
 `;
